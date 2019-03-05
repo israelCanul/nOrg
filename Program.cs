b490d7958b64b941211eebcbf4fc3bc153297355
Israel Canul <icanul@iran-imac.royalresorts.local>
2019-03-05 17:23:18 -0500

    configurando el appsettings para las variables de entorno

--- Program.cs
+++ Program.cs
@@ -14,11 +14,37 @@ namespace narilearsi
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+             CreateWebHostBuilder(args).Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHost CreateWebHostBuilder(string[] args) {
+            IWebHostBuilder builder = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory());
+
+
+            builder.ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                IHostingEnvironment env = hostingContext.HostingEnvironment;
+                config.AddJsonFile(
+                        "appsettings.json",
+                        optional: true,
+                        reloadOnChange: true
+                    ).AddJsonFile(
+                        $"appsettings.{env.EnvironmentName}.json",
+                        optional: true,
+                        reloadOnChange: true
+                    );
+                config.AddEnvironmentVariables();
+                if (args != null)
+                {
+                    config.AddCommandLine(args);
+                }                
+            });
+            
+            return builder.UseStartup<Startup>()
+                           .UseUrls("http://localhost:5020").Build();
+        }
+            
     }
 }
