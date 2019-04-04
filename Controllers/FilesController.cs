9440a5a93dc81a8f766a6010370551ae307ade60
Israel Canul <icanul@iran-imac.royalresorts.local>
2019-04-04 16:34:01 -0500

    adding imagesharp

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -47,5 +47,37 @@ namespace narilearsi.Controllers
             return Ok(new { count = files.Count, size, filePath });
         }
 
+		// [HttpPost("upload")]
+  //       public async Task<IActionResult> Post(List<IFormFile> files)
+  //       {
+  //           long size = files.Sum(f => f.Length);
+
+
+  //           // full path to file in temp location
+  //           var filePath = @"/Users/icanul/Documents/RepositoriosRoyal";
+  //           var outputStream = new MemoryStream();
+  //           foreach (var formFile in files)
+  //           {
+  //               if (formFile.Length > 0)
+  //               {
+
+  //                   using (var inputStream = formFile.OpenReadStream())
+  //                   using (Image<Rgba32> image = Image.Load(inputStream))
+  //                   {
+  //                       image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2).Grayscale());
+  //                       image.SaveAsJpeg<Rgba32>(outputStream);
+  //                   }
+  //                   outputStream.Seek(0, SeekOrigin.Begin);
+  //               }
+  //           }
+
+  //           // process uploaded files
+  //           // Don't rely on or trust the FileName property without validation.
+
+  //           return File(outputStream, "image/jpg");
+
+  //       }
+
+
     }
 }
